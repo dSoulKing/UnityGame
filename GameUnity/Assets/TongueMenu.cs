31f41360cc90134959fc26a3c26237ff78863cbf
dSoulKing <thomas.deray@gmail.com>
2018-05-18 11:51:17 +0200

    Menu burger animation OK

---
+++ GameUnity/Assets/TongueMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TongueMenu : MonoBehaviour, IPointerDownHandler {
+
+    public GameObject image;
+
+    private Animator animator;
+    private bool isOpen = false;
+
+    private void Start()
+    {
+        animator = image.GetComponent<Animator>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isOpen = !isOpen;
+        Debug.Log(isOpen);
+        animator.SetBool("isOpen", isOpen);
+    }
+}
