83629f104f509534cf09fd7838d6191a93d895e1
dSoulKing <thomas.deray@gmail.com>
2018-05-29 09:24:43 +0200

    Ajout de animation test

---
+++ AnimationTests/Assets/ControllerRyu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerRyu : MonoBehaviour {
+
+    public static bool zangiefAttack = false;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (zangiefAttack)
+        {
+            animator.SetBool("ZangiefAttack", zangiefAttack);
+            zangiefAttack = !zangiefAttack;
+        }
+        Debug.Log(zangiefAttack);
+    }
+}
---
+++ AnimationTests/Assets/ControllerZangief.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerZangief : MonoBehaviour {
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnMouseDown()
+    {
+            animator.SetTrigger("clickToAttack");
+            ControllerRyu.zangiefAttack = !ControllerRyu.zangiefAttack;
+
+    }
+}
